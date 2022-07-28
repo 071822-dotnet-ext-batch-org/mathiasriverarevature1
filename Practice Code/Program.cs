c4af48df6530b892fe3d63b6d47a0229478944c6
DESKTOP-8EBH9SK <mathias750@revature.net>
2022-07-28 15:08:02 -0500

    RpsConsole stuff and Practice Code

---
+++ Practice Code/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Practice_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] animals = {"dog","tiger","dolphin", "alligator","polar bear"};
+
+            for (int i = 0; i < animals.Length;i++)
+           {
+                for (int j = 0; j < animals[i].Length; j++)
+                {
+                    if (animals[i][j] == 'l')
+                    {
+                        Console.WriteLine(animals[i] + "");
+                    }
+                }
+            }
+            
+
+    }
+}
+}
