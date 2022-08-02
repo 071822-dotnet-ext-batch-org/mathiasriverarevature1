7a0eb15020010c801f236337d59827e5bf0621ff
DESKTOP-8EBH9SK <mathias750@revature.net>
2022-08-02 10:40:32 -0500

    Calc edits

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,22 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string UserOperation;
+            string Num1Str;
+            string Num2Str;
+            int Num1;
+            int Num2;
+            bool OperLoop = true;
+            bool Num1Loop = true;
+            bool Num2Loop = true;           
+           
+            //Get user opertation
+            Console.WriteLine("Welcome to the calculator! Let's start with inputting an operation. /n Please use either +,-,*,/.");
+            Num1Str = Console.ReadLine();
+
+            //get user number 1
+
+            //get user number 2
         }
     }
 }
