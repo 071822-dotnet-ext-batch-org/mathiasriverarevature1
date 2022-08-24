b453f9473b1616112f640b07a20b13e759ab33c6
DESKTOP-8EBH9SK <mathias750@revature.net>
2022-08-24 09:07:06 -0500

    code quiz scrap code

--- ScrapCode/Program.cs
+++ ScrapCode/Program.cs
@@ -1,28 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-
+using System.Linq;
 
 namespace ScrapCode
 {
     class Program
     {
-    static void Main(string[] args){
-        string[] strings = {"apple", "banana", "plum", "strawberry"};
-        Console.WriteLine(findOccurences(strings));    
+    static void Main(string[] args)
+    {
+    int[] theArray = { 3, 1, 3, 5, 7, 9 };
+    List<int> SortedList = theArray.ToList();
+    SortedList.Sort();
+    int total = 0;
+    for(int i=0; i < (SortedList.Count-1); i++)
+    {
+        int diff = Math.Abs(SortedList[i]-SortedList[i+1]);
+        Console.WriteLine(SortedList[i]+" "+SortedList[i+1]);
+        total += diff;
+        Console.WriteLine(total);
+        
     }
-     
-    static int findOccurences(string[] sArray){
-  	    int counter = 0;
-        foreach(string str in sArray){
-    	    foreach(char ch in str){
-        	    if (ch == 'a'){
-            	    counter++;
-                }
-            }
-        }
-        return counter;
-    }
-} 
+    Console.WriteLine(SortedList.IndexOf(3));
+
+    
+    
+    }   
+    } 
 }  
 
